4b541e0caa3bbd8bd80b2e9f054bc3f89ec27013
Fonzi <alfonso.vazquez@protonmail.com>
2019-04-20 21:57:00 -0500

    Fixed dropdowns

--- javascript/CreateProject.js
+++ javascript/CreateProject.js
@@ -14,11 +14,12 @@ function create()
     projectNCell.innerHTML       = '<td><input type="text" class="form-control mr-sm-2" placeholder="Ass Project Name" name="projectName" ></td>'
     clientCell.innerHTML         = '<td><input type="text" class="form-control mr-sm-2" placeholder="Ass Client" name="client"></td>'
     projectCell.innerHTML        = '<td><input type="text" class="form-control mr-sm-2" placeholder="Ass Project" name="project"></td>'
-    typeCell.innerHTML           = '<td><input type="text" class="form-control mr-sm-2" placeholder="Ass Type" name="Type"></td>'
+    typeCell.innerHTML           = '<td><select class="custom-select mr-sm-2" id="inlineFormCustomSelect"><option selected>Choose...</option><option value="1">ONE</option><option value="2">TWO</option><option value="3">THREE</option><option value="4">FOUR</option><option value="5">FIVE</option></select></td>'
     keywordsCell.innerHTML       = '<td><input type="text" class="form-control mr-sm-2" placeholder="Ass Keywords" name="Keywords"></td>'
     projectPaletteCell.innerHTML = '<td><input type="color" class="form-control mr-sm-2" name="color"></td>'
     dueDateCell.innerHTML        = '<td><input type="date" class="form-control mr-sm-2" name="Due Date"></td>'
     deleteCellButton.innerHTML   = '<td><button class="btn btn-danger" onclick="delRow(this)">Delete</button>'
+
 }
 
 function delRow(x)

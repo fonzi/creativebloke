9b9ca9d292f357676b8af2ff93083c4f47dda542
alfonso.vazquez <626330+fonzi@users.noreply.github.com>
2019-04-21 17:23:41 -0500

    Fixed spelling

--- javascript/CreateProject.js
+++ javascript/CreateProject.js
@@ -11,11 +11,11 @@ function create()
     var dueDateCell = row.insertCell(6);
     var deleteCellButton = row.insertCell(7);
 
-    projectNCell.innerHTML       = '<td><input type="text" class="form-control mr-sm-2" placeholder="Ass Project Name" name="projectName" ></td>'
-    clientCell.innerHTML         = '<td><input type="text" class="form-control mr-sm-2" placeholder="Ass Client" name="client"></td>'
-    projectCell.innerHTML        = '<td><input type="text" class="form-control mr-sm-2" placeholder="Ass Project" name="project"></td>'
+    projectNCell.innerHTML       = '<td><input type="text" class="form-control mr-sm-2" placeholder="Project Name" name="projectName" ></td>'
+    clientCell.innerHTML         = '<td><input type="text" class="form-control mr-sm-2" placeholder="Client" name="client"></td>'
+    projectCell.innerHTML        = '<td><input type="text" class="form-control mr-sm-2" placeholder="Project" name="project"></td>'
     typeCell.innerHTML           = '<td><select class="custom-select mr-sm-2" id="inlineFormCustomSelect"><option selected>Choose...</option><option value="1">ONE</option><option value="2">TWO</option><option value="3">THREE</option><option value="4">FOUR</option><option value="5">FIVE</option></select></td>'
-    keywordsCell.innerHTML       = '<td><input type="text" class="form-control mr-sm-2" placeholder="Ass Keywords" name="Keywords"></td>'
+    keywordsCell.innerHTML       = '<td><input type="text" class="form-control mr-sm-2" placeholder="Keywords" name="Keywords"></td>'
     projectPaletteCell.innerHTML = '<td><input type="color" class="form-control mr-sm-2" name="color"></td>'
     dueDateCell.innerHTML        = '<td><input type="date" class="form-control mr-sm-2" name="Due Date"></td>'
     deleteCellButton.innerHTML   = '<td><button class="btn btn-danger" onclick="delRow(this)">Delete</button>'
